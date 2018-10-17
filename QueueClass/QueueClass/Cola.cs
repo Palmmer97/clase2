b86a0d76afe1561293c9184fd130421c6d7d64b8
PC1\65676 <alexwolfs997@gmail.com>
2018-10-16 19:40:46 -0700

    eso

---
+++ QueueClass/QueueClass/Cola.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueClass
+{
+   public class Cola
+    {
+       private ArrayList pcola;
+       // Crear la lista 
+       public Cola()
+       {
+           pcola = new ArrayList();
+
+       }
+       // Agrega el objeto de entrada al arraylist
+       // el objeto puede ser cualquier tipo de dato.
+       public void EnQueue(object item)
+       {
+           pcola.Add(item);
+       }
+       //Elimina la posicion 0 de la lista permitiendo sacar ese elemento
+ 
+       public void DeQueue()
+       {
+           pcola.RemoveAt(0);
+       }
+       //peek deja ver el ultimo elemento 
+       public object Peek()
+       {
+           return pcola[0];
+       }
+       public void ClearQueue()
+       {
+           //vacia los elementos de la cola 
+           pcola.Clear();
+
+       }
+       //Regresa la cantidad de elementos de 1 a cola 
+       public int Count()
+       {
+           return pcola.Count;
+       }
+   
+
+
+    }
+
+}
---
+++ QueueClass/QueueClass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cola miCola = new Cola();
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Indroduce los elementos{0} de la cola", i+1);
+                var Elem = Console.ReadLine();
+                miCola.EnQueue(Elem);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(miCola.Peek());
+                miCola.DeQueue();
+            }
+            Console.ReadKey();
+            // Inicializacion de las colas con datos 
+            Queue queue = new Queue(100);
+            // capacidad y el factor de crecimiento de la cola 
+            Queue miQueue = new Queue(100, 3);
+            //Cola con tipo de dato especifico 
+            Queue<int> numeros = new Queue<int>();
+
+            Queue mico = new Queue();
+
+            
+        }
+    }
+}
